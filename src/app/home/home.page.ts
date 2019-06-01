20f45ac5f0ca5821459336ad4da88ecdd7fd4f14
Marco.Aurelio <marcorodriguessantosjr@gmail.com>
2019-05-31 21:40:10 -0300

    teste github

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Carro } from 'src/modelos/Carro';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public carros : Carro[];
+  
+  constructor() {
+    this.carros = [
+      {nome: 'Corvette', preco: '250000', cor: 'Branco'},
+      {nome: 'Opala', preco: '10000', cor: 'Preto'}
+    ]; 
+  }
+
+}
